bafbd22d67692a344a4fc60ad77c5992cf9a1ee2
dheerajsatyavarapu <dheerajdheeru126s@gmail.com>
2023-02-10 17:22:10 +0530

    Initial commit

---
+++ telegram_app/config.py
@@ -0,0 +1,2 @@
+BOT_URL = "t.me/pft_finance_made_easy_bot"
+TOKEN = "5868599261:AAEetO2yp3WPQK3VFyIuU9J9t7hr7Yz4wxw"
---
+++ telegram_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler
+# Create your views here.
